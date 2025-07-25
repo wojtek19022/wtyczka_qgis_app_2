ca492c8580bbd314c0e1b2b03cae5a2c68cb079f
wojtek19022 <113895660+wojtek19022@users.noreply.github.com>
2025-07-25 18:41:16 +0200

    dodanie brakujacych folderow

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker compose -f docker-compose.yml up -d
+docker compose -f docker-compose.yml up -d --force-recreate
 echo 'Wait 10 seconds'
 sleep 10
 echo 'Installation of the plugin'
